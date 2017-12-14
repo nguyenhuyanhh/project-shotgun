8b3490becb1e39c746db2adbbb82547fc741265d
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-12-14 11:30:43 +0800

    Change class name for extracted divs to userContentWrapper

--- synthesis_technical_challenge.py
+++ synthesis_technical_challenge.py
@@ -1,17 +1,19 @@
+"""Technical challenge for Synthesis.
+
+TASK: Create a code thats prints each the post's status and all other useful information
+in the post in a manner similar to the original facebook page (separate each post with a newline)
+Website: https://www.facebook.com/bobbibrown.sg/
+"""
+
+from __future__ import print_function
+
 import requests
 from bs4 import BeautifulSoup
 
 page = requests.get("https://www.facebook.com/bobbibrown.sg/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#Gets all the posts in the page:
-
-divs = soup.find_all('div', class_="_5pcr fbUserStory")
+# Gets all the posts in the page
+divs = soup.find_all('div', class_="_5pcr userContentWrapper")
 for div in divs:
     print(div)
-
-# TASK: Create a code thats prints each the post's status and all other useful information in the post in a manner similar to the original facebook page (separate each post with a newline) 
-# Website: https://www.facebook.com/bobbibrown.sg/
-
-
-
