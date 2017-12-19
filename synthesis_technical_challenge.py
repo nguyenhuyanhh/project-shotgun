9c5d4b3af3abbcfa6cfa4c136267bb5d2e1bd9c1
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-12-19 20:21:50 +0800

    Initial implementation of Facebook Graph API

--- synthesis_technical_challenge.py
+++ synthesis_technical_challenge.py
@@ -7,13 +7,37 @@ Website: https://www.facebook.com/bobbibrown.sg/
 
 from __future__ import print_function
 
+import json
+import os
+
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.facebook.com/bobbibrown.sg/")
-soup = BeautifulSoup(page.content, 'html.parser')
+CUR_DIR = os.path.dirname(os.path.realpath(__file__))
+CONFIG_PATH = os.path.join(CUR_DIR, 'config.json')
 
-# Gets all the posts in the page
-divs = soup.find_all('div', class_="_5pcr userContentWrapper")
-for div in divs:
-    print(div)
+
+def old_crawler():
+    """Crawling the Facebook page using BeautifulSoup."""
+    page = requests.get("https://www.facebook.com/bobbibrown.sg/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Gets all the posts in the page
+    divs = soup.find_all('div', class_="_5pcr userContentWrapper")
+    for div in divs:
+        print(div)
+
+
+def new_crawler():
+    """Crawling the Facebook page using Facebook Graph API."""
+    # Get access token from config.json
+    with open(CONFIG_PATH, 'r') as json_:
+        config = json.load(json_)
+
+    url = 'https://graph.facebook.com/v2.11/bobbibrown.sg/posts'
+    response = requests.get(url=url, params=config).json()
+    print(response)
+
+
+if __name__ == '__main__':
+    new_crawler()
