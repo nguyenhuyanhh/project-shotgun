9b7a7c1a85f6c757f87c47715008a7b990086672
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-12-20 10:17:02 +0800

    Fix share count, add running time measurement and csv output

--- synthesis_technical_challenge.py
+++ synthesis_technical_challenge.py
@@ -11,18 +11,19 @@ import json
 import os
 
 import requests
-from bs4 import BeautifulSoup
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 CONFIG_PATH = os.path.join(CUR_DIR, 'config.json')
 TEMP_PATH = os.path.join(CUR_DIR, 'temp.json')
 
 GRAPH_API_HEADER = 'https://graph.facebook.com/v2.11/'
-GRAPH_API_REACTIONS = ['LOVE', 'WOW', 'HAHA', 'SAD', 'ANGRY', 'THANKFUL']
+GRAPH_API_REACTIONS = ['LOVE', 'WOW', 'HAHA', 'SAD', 'ANGRY']
 
 
 def old_crawler():
     """Crawling the Facebook page using BeautifulSoup."""
+    from bs4 import BeautifulSoup
+
     page = requests.get("https://www.facebook.com/bobbibrown.sg/")
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -34,6 +35,8 @@ def old_crawler():
 
 def new_crawler(limit=100):
     """Crawling the Facebook page using Facebook Graph API."""
+    import pandas as pd
+
     # Get access token from config.json
     with open(CONFIG_PATH, 'r') as json_:
         config = json.load(json_)
@@ -43,13 +46,13 @@ def new_crawler(limit=100):
     data = []
     url = GRAPH_API_HEADER + 'bobbibrown.sg/posts'
     response = requests.get(url=url, params=config).json()
-    while len(data) <= limit and 'paging' in response and 'next' in response['paging']:
+    while len(data) < limit and 'paging' in response and 'next' in response['paging']:
         data += response['data']
         response = requests.get(url=response['paging']['next']).json()
 
     # Get post data from Graph API
-    config['fields'] = 'likes.summary(1),comments.summary(1),sharedposts,'
-    config['fields'] += ','.join(['reactions.type({}).summary(1).as(reactions_{})'.format(
+    config['fields'] = 'likes.summary(1),comments.summary(1),shares,'
+    config['fields'] += ','.join(['reactions.type({}).limit(0).summary(1).as(reactions_{})'.format(
         x, x.lower()) for x in GRAPH_API_REACTIONS])
     for post in data:
         url = GRAPH_API_HEADER + post['id']
@@ -59,13 +62,22 @@ def new_crawler(limit=100):
             post[reaction_id] = response[reaction_id]['summary']['total_count']
         post['reactions_like'] = response['likes']['summary']['total_count']
         post['comments'] = response['comments']['summary']['total_count']
-        if 'sharedposts' in response:  # sharedposts only available for some post types
-            post['shared'] = response['sharedposts']
+        if 'shares' in response:  # shares only available for some post types
+            post['shares'] = response['shares']['count']
 
     # Output data to temporary file
     with open(TEMP_PATH, 'w') as json_:
         json.dump(data, json_, indent=4, sort_keys=True)
 
+    # Output data to csv
+    df_ = pd.DataFrame(data)
+    df_.to_csv(os.path.join(CUR_DIR, 'output.csv'), index=False)
+
 
 if __name__ == '__main__':
+    from time import time
+
+    # Measure running time
+    START_TIME = time()
     new_crawler(100)
+    print('Running time: {} secs'.format(time() - START_TIME))
